bed6cc63c0e9c68e2f27e33c94898e82eab8fa54
Allan Ritchie <allan.ritchie@gmail.com>
2015-07-20 22:30:35 -0400

    Samples started

---
+++ Samples/Samples/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+
+namespace Samples {
+
+    public class App : Application {
+
+        public App() {
+        }
+
+
+        protected override void OnStart() {
+        }
+
+
+        protected override void OnSleep() {
+        }
+
+
+        protected override void OnResume() {
+        }
+    }
+}
