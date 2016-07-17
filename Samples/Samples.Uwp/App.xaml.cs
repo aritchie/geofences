1168df5edb02d9620aa7585c689b499aa10aee71
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-17 15:17:23 -0400

    add uwp samples

---
+++ Samples/Samples.Uwp/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.ApplicationInsights;
+using Xamarin.Forms;
+using Application = Windows.UI.Xaml.Application;
+using Frame = Windows.UI.Xaml.Controls.Frame;
+
+
+namespace Samples.Uwp
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            WindowsAppInitializer.InitializeAsync(WindowsCollectors.Metadata | WindowsCollectors.Session);
+            this.InitializeComponent();
+            this.Suspending += this.OnSuspending;
+        }
+
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += this.OnNavigationFailed;
+                Forms.Init(e);
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                }
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            Window.Current.Activate();
+        }
+
+
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+
+        void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            e.SuspendingOperation.GetDeferral().Complete();
+        }
+    }
+}
