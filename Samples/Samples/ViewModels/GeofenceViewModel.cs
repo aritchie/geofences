c4a36ab48be15a4c465576aacd45455a51f057a6
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-18 22:14:29 -0400

    list commands for geofence settings

---
+++ Samples/Samples/ViewModels/GeofenceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+using Acr.Geofencing;
+
+namespace Samples
+{
+    public class GeofenceViewModel
+    {
+        readonly GeofenceRegion region;
+
+
+        public GeofenceViewModel(GeofenceRegion region, ICommand removeCommand) 
+        {
+            this.Region = region;
+            this.Remove = removeCommand;
+        }
+
+
+        public GeofenceRegion Region { get; }
+        public ICommand Remove { get; }
+    }
+}
+
