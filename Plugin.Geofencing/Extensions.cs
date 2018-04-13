dd1d1cef5dd3c8af67d1060dd7289c8c01fdf497
Allan Ritchie <allan.ritchie@gmail.com>
2018-04-13 16:04:29 -0400

    extensions

---
+++ Plugin.Geofencing/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Plugin.Geofencing
+{
+    public static class Extensions
+    {
+        public static void StopMonitoring(this IGeofenceManager geofenceMgr, string identifier)
+        {
+            foreach (var region in geofenceMgr.MonitoredRegions)
+            {
+                if (region.Identifier.Equals(identifier, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    geofenceMgr.StopMonitoring(region);
+                    break;
+                }
+            }
+        }
+
+
+        public static bool IsPositionInside(this GeofenceRegion region, Position position)
+        {
+            var distance = region.Center.GetDistanceTo(position);
+            var inside = distance.TotalMeters <= region.Radius.TotalMeters;
+            return inside;
+        }
+    }
+}
