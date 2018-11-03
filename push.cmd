381f1353e0902fec3280a2a56cd4d098678cd142
Allan Ritchie <allan.ritchie@gmail.com>
2018-11-03 08:32:01 -0400

    update push

--- push.cmd
+++ push.cmd
@@ -1,4 +1,6 @@
+
 @echo off
 nuget push .\Plugin.Geofencing\bin\Release\*.nupkg -Source https://www.nuget.org/api/v2/package
 nuget push .\Plugin.Geofencing\bin\Release\*.nupkg -Source https://www.myget.org/F/acr/api/v2/package
+del .\Plugin.Geofencing\bin\Release\*.nupkg
 pause
